49c90d6f9f3d422b50ce85b92a59934aab9fb557
ThioKDG <kim2006207@gmail.com>
2023-08-17 20:22:04 +0900

    헤더부분 조정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,72 +4,70 @@ import { Link } from "react-router-dom";
 import "./layStyles/layHaeder.scss";
 
 function Header(props) {
-   const [head, setHead] = useState(false);
-   const [isOpen, setOpen] = useState(false);
-   const refHead = useRef();
-   const refNavi = useRef();
-   const refBtn = useRef();
+  const [head, setHead] = useState(false);
+  const [isOpen, setOpen] = useState(false);
+  const refHead = useRef();
+  const refNavi = useRef();
+  const refBtn = useRef();
 
-   useEffect(() => {
-      const wheelHandler = (e) =>{
-         // e.deltaY >= 100 ? setHead(true) : setHead(false);
-         // console.log(window.scrollY);
-         if(window.scrollY >= 100 ){
-            setHead(true);
-         }else{
-            setHead(false);
-         }
+  useEffect(() => {
+    const wheelHandler = (e) => {
+      // e.deltaY >= 100 ? setHead(true) : setHead(false);
+      // console.log(window.scrollY);
+      if (window.scrollY >= 1000) {
+        setHead(true);
+      } else {
+        setHead(false);
       }
-      /* 휠 이벤트 발생시 deltaY 값을 받아서 헤더를 숨겨줌 */
-      window.addEventListener("wheel", wheelHandler);
+    };
+    /* 휠 이벤트 발생시 deltaY 값을 받아서 헤더를 숨겨줌 */
+    window.addEventListener("wheel", wheelHandler);
 
-      return () =>  window.removeEventListener("wheel", wheelHandler);
-   }, []);
-   
-   const toggleMenu = () => {
-      setOpen(isOpen => !isOpen);
-   };
+    return () => window.removeEventListener("wheel", wheelHandler);
+  }, []);
 
-   const handleGoUpProject = () => {
-      window.scrollTo({
-         top: 950,
-         behavior: "smooth",
-      });
-   };
-   const handleGoUpSkill = () => {
-      window.scrollTo({
-         top: 2400,
-         behavior: "smooth",
-      });
-   };
-   const handleGoUpFooter = () => {
-      window.scrollTo({
-         top: 3800,
-         behavior: "smooth",
-      });
-   };
+  const toggleMenu = () => {
+    setOpen((isOpen) => !isOpen);
+  };
 
-   return (
-      <header className={head ? "headerContianer scrolled" : "headerContianer"}>
-            <nav>
-               <div className="navInner">
-                  <h1 className='headerLogoH1'>
-                     <Link to='./'>portfolio</Link> 
-                  </h1>
-                  <ul className={isOpen ? "headNaviList on" : "headNaviList"}>
-                     <li onClick={handleGoUpProject}>project</li>
-                     <li onClick={handleGoUpSkill}>skill</li>
-                     <li onClick={handleGoUpFooter}>about</li>
-                  </ul>
-                  <div onClick={()=>toggleMenu()} className="moMenuOpen">
-                     <p className="hiddenWord">
-                        메뉴 열림 버튼
-                     </p>
-                  </div>
-               </div>
-            </nav>
-      </header>
-   );
+  const handleGoUpProject = () => {
+    window.scrollTo({
+      top: 950,
+      behavior: "smooth",
+    });
+  };
+  const handleGoUpSkill = () => {
+    window.scrollTo({
+      top: 2400,
+      behavior: "smooth",
+    });
+  };
+  const handleGoUpFooter = () => {
+    window.scrollTo({
+      top: 3800,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <header className={head ? "headerContianer scrolled" : "headerContianer"}>
+      <nav>
+        <div className="navInner">
+          <h1 className="headerLogoH1">
+            <Link to="./">portfolio</Link>
+          </h1>
+          <ul className={isOpen ? "headNaviList on" : "headNaviList"}>
+            <li onClick={handleGoUpProject}>project</li>
+            <li onClick={handleGoUpSkill}>skill</li>
+            <li onClick={handleGoUpFooter}>about</li>
+          </ul>
+          <div onClick={() => toggleMenu()} className="moMenuOpen">
+            <p className="hiddenWord">메뉴 열림 버튼</p>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
