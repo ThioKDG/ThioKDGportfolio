abdb5518ec2fae9793a7b4f882b4bf740cedd501
ThioKDG <kim2006207@gmail.com>
2023-09-06 14:57:45 +0900

    0906 프로젝트 설명페이지 삭제 및 노션링크추가

--- src/components/MainProject.js
+++ src/components/MainProject.js
@@ -2,28 +2,35 @@ import { Link } from "react-router-dom";
 import "./compStyle/projectStyle.scss";
 
 function MainProject({ project }) {
-   const path = process.env.PUBLIC_URL;
+  const path = process.env.PUBLIC_URL;
 
-   return (
-      <div className="projectWrapper">
-         <ul class="projectList">
-            {project.map((item) => (
-               <li key={item.Id}>
-                  <Link to={`/sub${item.Id}`}>
-                     <p className="projectPhoto">
-                        <img src={path + `/image/main/${item.thumbnail}`} alt={item.projectName} />
-                     </p>
-                     <span className="langTit">{item.lang}</span>
-                     <h3 className="projectTit">{item.projectName}</h3>
-                     <p className="projectValue">{item.team}</p>
-                     <span className="dateTxt">{item.date}</span>
-                  </Link>
-               </li>
-            ))}
-         </ul>
-            
-      </div>
-   );
+  return (
+    <div className="projectWrapper">
+      <ul class="projectList">
+        {project.map((item) => (
+          <li key={item.Id}>
+            <p
+              onClick={() => {
+                window.open(`${item.link}`);
+              }}
+            >
+              <p className="projectPhoto">
+                <span className="hoverFloat">노션 바로가기</span>
+                <img
+                  src={path + `/image/main/${item.thumbnail}`}
+                  alt={item.projectName}
+                />
+              </p>
+              <span className="langTit">{item.lang}</span>
+              <h3 className="projectTit">{item.projectName}</h3>
+              <p className="projectValue">{item.team}</p>
+              <span className="dateTxt">{item.date}</span>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MainProject;
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,93 +1,96 @@
-import './compStyle/skillStyle.scss';
+import "./compStyle/skillStyle.scss";
 
 function Skill() {
-   return (
-      <section className="skillContainer">
-         <div className="skillWarpper">
-            {/* markup */}
-            <div className="skillItem">
-               <div className='itemWrapper'>
-                  <p className="skillBul">markUp</p>
-               </div>
-               <div className='techWrapper'>
-                  <article className="techIntro">
-                     <h3 className='techTit'>HTML5, CSS3</h3>
-                     <ul className="techInfo">
-                        <li>웹 접근성을 고려한 시멘틱 태그</li>
-                        <li>미디어 쿼리를 이용한 반응형 웹 제작</li>
-                        <li>CSS를 이용한 애니메이션 사용</li>
-                        <li>Flex, Grid, Position 이용</li>
-                        <li>전처리기 SCSS 사용</li>
-                     </ul> 
-                  </article>
-               </div>
-            </div>
-            {/* script react */}
-            <div className="skillItem">
-               <div className='itemWrapper'>
-                  <p className="skillBul">script</p>
-                  <p className="skillBul">react</p>
-               </div>
-               <div className='techWrapper'>
-                  <article className="techIntro">
-                     <h3 className='techTit'>JAVASCRIPT(ES6+)</h3>
-                     <ul className="techInfo">
-                        <li>ES6+ 문법 이해</li>
-                        <li>let, const 키워드 / 객체 리터럴 방식 사용</li>
-                        <li>구조 분해 할당 이용</li>
-                        <li>Map, Filter등 고차함수 이용</li>
-                        <li>웹 스토리지(Web Storge)</li>
-                        <li>로컬 스토리지를(local Storge) 를 이용하여 클라이언트에 저장 및 활용</li>
-                        <li>통신 Fetch API 및 Aixos 사용</li>
-                        <li>비동기적 통신</li>
-                        <li>setTimeout, setInterval, async, await</li>
-                     </ul> 
-                  </article>
-                  <article className="techIntro">
-                     <h3 className='techTit'>React</h3>
-                     <ul className="techInfo">
-                        <li>컴포넌트 생명주기에 대한 이해</li>
-                        <li>Hook</li>
-                        <li>use State, useREF, useEffect 사용</li>
-                        <li>React-router-dom</li>
-                     </ul> 
-                  </article>
-               </div>
-            </div>
-            {/* cooperation */}
-            <div className="skillItem">
-               <div className='itemWrapper'>
-                  <p className="skillBul">cooperation</p>
-               </div>
-               <div className="techWrapper">
-                  <article className="techIntro">
-                     <h3 className='techTit'>git &amp; github</h3>
-                     <ul className="techInfo">
-                        <li>bash, VS CODE 터미널 사용</li>
-                        <li>git을 통한 작업물 버전 관리</li>
-                        <li>gitHub를 통한 프로젝트 관리</li>
-                     </ul> 
-                  </article>
-               </div>
-            </div>
-            {/* graphic */}
-            <div className="skillItem">
-               <div className="itemWrapper">
-                  <p className="skillBul">graphic</p>
-               </div>
-               <div className="techWrapper">
-                  <article className="techIntro">
-                     <h3 className='techTit'>photoshop, illustrator, figma</h3>
-                     <ul className="techInfo">
-                        <li>이미지 편집</li>
-                        <li>프로토타입 제작</li>
-                     </ul> 
-                  </article>
-               </div>
-            </div>
-         </div>
-      </section>
-   );
+  return (
+    <section className="skillContainer">
+      <div className="skillWarpper">
+        {/* markup */}
+        <div className="skillItem">
+          <div className="itemWrapper">
+            <p className="skillBul">markUp</p>
+          </div>
+          <div className="techWrapper">
+            <article className="techIntro">
+              <h3 className="techTit">HTML5, CSS3</h3>
+              <ul className="techInfo">
+                <li>웹 접근성을 고려한 시멘틱 태그</li>
+                <li>미디어 쿼리를 이용한 반응형 웹 제작</li>
+                <li>CSS를 이용한 애니메이션 사용</li>
+                <li>Flex, Grid, Position 이용</li>
+                <li>전처리기 SCSS 사용</li>
+              </ul>
+            </article>
+          </div>
+        </div>
+        {/* script react */}
+        <div className="skillItem">
+          <div className="itemWrapper">
+            <p className="skillBul">script</p>
+            <p className="skillBul">framework</p>
+          </div>
+          <div className="techWrapper">
+            <article className="techIntro">
+              <h3 className="techTit">JAVASCRIPT(ES6+)</h3>
+              <ul className="techInfo">
+                <li>ES6+ 문법 이해</li>
+                <li>let, const 키워드 / 객체 리터럴 방식 사용</li>
+                <li>구조 분해 할당 이용</li>
+                <li>Map, Filter등 고차함수 이용</li>
+                <li>웹 스토리지(Web Storge)</li>
+                <li>
+                  로컬 스토리지를(local Storge) 를 이용하여 클라이언트에 저장 및
+                  활용
+                </li>
+                <li>통신 Fetch API 및 Aixos 사용</li>
+                <li>비동기적 통신</li>
+                <li>setTimeout, setInterval, async, await</li>
+              </ul>
+            </article>
+            <article className="techIntro">
+              <h3 className="techTit">React</h3>
+              <ul className="techInfo">
+                <li>컴포넌트 생명주기에 대한 이해</li>
+                <li>Hook</li>
+                <li>use State, useREF, useEffect 사용</li>
+                <li>React-router-dom</li>
+              </ul>
+            </article>
+          </div>
+        </div>
+        {/* cooperation */}
+        <div className="skillItem">
+          <div className="itemWrapper">
+            <p className="skillBul">cooperation</p>
+          </div>
+          <div className="techWrapper">
+            <article className="techIntro">
+              <h3 className="techTit">git &amp; github</h3>
+              <ul className="techInfo">
+                <li>bash, VS CODE 터미널 사용</li>
+                <li>git을 통한 작업물 버전 관리</li>
+                <li>gitHub를 통한 프로젝트 관리</li>
+              </ul>
+            </article>
+          </div>
+        </div>
+        {/* graphic */}
+        <div className="skillItem">
+          <div className="itemWrapper">
+            <p className="skillBul">graphic</p>
+          </div>
+          <div className="techWrapper">
+            <article className="techIntro">
+              <h3 className="techTit">photoshop, illustrator, figma</h3>
+              <ul className="techInfo">
+                <li>이미지 편집</li>
+                <li>프로토타입 제작</li>
+              </ul>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Skill;
