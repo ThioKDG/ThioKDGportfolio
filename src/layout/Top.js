ac2b5685a3f6b79260a06a8798b2f2c7516ec2cc
ThioKDG <kim2006207@gmail.com>
2023-10-05 13:12:17 +0900

    1005 메인문구 변경

--- src/layout/Top.js
+++ src/layout/Top.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 import "./layStyles/layTops.scss";
 
 function Top(props) {
   const path = process.env.PUBLIC_URL;
   return (
     <section className="introTop">
-      <h2 className="topTit">frontend</h2>
+      <h2 className="topTit">webdev</h2>
       <video
         className="topVideo"
         src={path + `/image/main/walk.mp4`}
